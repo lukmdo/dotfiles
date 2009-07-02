82a941da876ad8a3c637957fbcfef663e529ffa9
Lukasz Dobrzanski <lukasz.m.dobrzanski@gmail.com>
2009-07-02 20:08:27 +0200

    Extended syntax_checking script.

--- .scripts/common.sh
+++ .scripts/common.sh
@@ -11,12 +11,14 @@ sc () {
   for FILE in $*; do 
     ft=`echo $FILE |awk -F. '{print $(NF)}'`
     case $ft in
-      'pl'  ) perl -wc $FILE;;
-      'py'  ) ERRORS=`python -c "import py_compile; py_compile.compile(r'$FILE')"`
-              if [ -z "$ERRORS" ]; then echo "$FILE syntax OK";else echo $ERROR; fi;;
-      'sh'  ) ERRORS="`bash -n "$FILE"`"
-              if [ -z "$ERRORS" ]; then echo "$FILE syntax OK";else echo $ERROR; fi;;
-       *    ) echo "Unknown file type: $ft of $FILE";;
+      *.pl|*.pm ) perl -wc $FILE;;
+      *.py      ) ERRORS=`python -c "import py_compile; py_compile.compile(r'$FILE')"`
+                  if [ -z "$ERRORS" ]; then echo "$FILE syntax OK";else echo $ERROR; fi;;
+      *.sh      ) ERRORS="`bash -n "$FILE"`"
+                  if [ -z "$ERRORS" ]; then echo "$FILE syntax OK";else echo $ERROR; fi;;
+      *.rb      ) ERRORS="`ruby -c "$FILE"`"
+                  if [ -z "$ERRORS" ]; then echo "$FILE syntax OK";else echo $ERROR; fi;;
+      *         ) echo "Unknown file type: $ft of $FILE";;
     esac
   done
 }
