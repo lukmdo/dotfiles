a6499252735e7e2bb897f5fe5c38ece06ce6f74f
Lukasz Dobrzanski <code@lukmdo.com>
2025-04-29 01:04:40 +0100

    more macos/set_defaults

--- macos/set_defaults.sh
+++ macos/set_defaults.sh
@@ -12,3 +12,25 @@ defaultbrowser chrome
 # defaults export pbs - | plutil -convert json -o - -  | jq --sort-keys > macos/defaults.pbs.json
 plutil -convert xml1 -o - macos/defaults.pbs.json | defaults import pbs -
 /System/Library/CoreServices/pbs -flush
+
+
+# https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+
+# Set max keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
+
+# Show the /Volumes folder
+sudo chflags nohidden /Volumes
+
+# Prevent Photos from opening automatically when devices are plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+# Disable automatic emoji substitution (i.e. use plain text smileys)
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticEmojiSubstitutionEnablediMessage" -bool false
